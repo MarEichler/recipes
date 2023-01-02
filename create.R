b2695696d52a4d16cb2c43e6ae289aaeb97c6f2a
MarEichler <mareichler@protonmail.com>
2023-01-02 14:58:37 -0600

    re-run and update min page width for html version

--- create.R
+++ create.R
@@ -1,3 +1,4 @@
 
 
-pagedown::chrome_print("recipe_book.Rmd")
+pagedown::chrome_print("recipe_book.Rmd", output = "recipe_book_print.pdf")
+file.copy(from = "recipe_book.html", to = "index.html", overwrite = TRUE)
